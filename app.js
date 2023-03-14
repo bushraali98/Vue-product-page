3b0673e48e0cf0d188f21d86bdcb07537936ad80
bushraali98 <bm-bisho-90210@hotmail.com>
2023-03-14 12:22:56 +0300

    added button functionality

--- app.js
+++ app.js
@@ -2,41 +2,20 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Fall Limited Edition Sneakers',
-        images: [
-            {
-                id: 1,
-                img: 'assets/img1.jpg'
-            },
-            {
-                id: 2,
-                img: 'assets/img2.jpg'
-            },
-            {
-                id: 3,
-                img: 'assets/img3.jpg'
-            },
-            {
-                id: 4,
-                img: 'assets/img4.jpg'
-            }
-
-            ],
-            img1: 'assets/img1.jpg',
-            img2: 'assets/img2.jpg',
-            img3: 'assets/img3.jpg',
-            img4: 'assets/img4.jpg',
-        currentImage: 'assets/img1.jpg'
+        img1: 'assets/img1.jpg',
+        img2: 'assets/img2.jpg',
+        img3: 'assets/img3.jpg',
+        img4: 'assets/img4.jpg',
+        count: 0,
     },
-    // methods: {
-    //     changeImg() {
-    //         currentImage === 
-    //     }
-    // }
+    methods: {
+        increamentProduct() {
+            this.count += 1
+        },
+        decreamentProduct () {
+            if (this.count > 0 ) {
+                this.count -= 1
+            }
+        }
+    }
 })
-// function currentImg(imgs) {
-//     var expandImg = document.getElementById("expandedImg");
-//     var imgText = document.getElementById("imgtext");
-//     expandImg.src = imgs.src;
-//     imgText.innerHTML = imgs.alt;
-//     expandImg.parentElement.style.display = "block";
-// }
